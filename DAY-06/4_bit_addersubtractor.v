0a6c68f42881704e360b320c5c40f7e26bbac38c
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-06-19 19:52:05 +0530

    Update 4_bit_addersubtractor.v

--- DAY-06/4_bit_addersubtractor.v
+++ DAY-06/4_bit_addersubtractor.v
@@ -7,7 +7,6 @@ input mode;
 output[3:0]sum;
 output cout;
 
-
 wire w1,w2,w3,w4;
 wire c1,c2,c3;
 
@@ -20,7 +19,6 @@ full_adder f1(a[0],w1,mode,sum[0],c1);
 full_adder f2(a[1],w2,c[0],sum[1],c2);
 full_adder f3(a[2],w3,c[1],sum[2],c3);
 full_adder f4(a[3],w4,c[2],sum[3],cout);
-
 endmodule
 
 
