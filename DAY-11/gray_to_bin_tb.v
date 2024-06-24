f4afa760be9157f42dd77610f01b8b0928df2285
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-06-24 19:32:31 +0530

    Update gray_to_bin_tb.v

--- DAY-11/gray_to_bin_tb.v
+++ DAY-11/gray_to_bin_tb.v
@@ -2,16 +2,12 @@
 
 module gray_to_binary_tb();
 
-
 reg [3:0]gray;
 wire [3:0]binary;
 
 gray_2_binary_conveter uut(.binary(binary),.gray(gray));
-
-
-
+  
 initial begin
-
 repeat (10) begin
 gray = $random;
 #10;
