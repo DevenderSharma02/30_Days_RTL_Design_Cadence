3236db3ba19bd98fe96c2e563164a230b30452ef
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-06-28 20:06:57 +0530

    Update demux_1to8_tb.v

--- DAY-15/demux_1to8_tb.v
+++ DAY-15/demux_1to8_tb.v
@@ -5,16 +5,16 @@ reg data;
 reg [2:0] sel;
 wire [7:0] y;
 
-demux_1x8 uut(.data(data), .sel(sel), .y(y));
+demux_1x8 uut(.data(data), 
+              .sel(sel), 
+              .y(y)
+              );
 
 initial begin
 repeat (10) begin
-
 data = $random;
 sel = $random;
 #10;
 end
-$finish;
-
 end
 endmodule
