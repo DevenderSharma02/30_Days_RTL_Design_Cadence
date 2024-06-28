62c42dd992027fefb18c936c5d0eeb55f542cdd3
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-06-28 19:42:31 +0530

    Update demux_1x4_df.v

--- DAY-14/demux_1x4_df.v
+++ DAY-14/demux_1x4_df.v
@@ -5,9 +5,9 @@ module demux_1x4(data,sel,y);
   input [1:0] sel;
   output [3:0] y;
 
-  assign y[0] = (~sel[0]) & (~sel[1]) & data;
-  assign y[1] =  sel[0] & (~sel[1]) & data;
-  assign y[2] = (~sel[0]) & sel[1] & data;
-  assign y[3] = sel[0] & sel[1] & data;
+  assign y[0] = data & ~sel[1] & ~sel[0];  // Y0
+  assign y[1] = data & ~sel[1] & sel[0];  // Y1
+  assign y[2] = data & sel[1] & ~sel[0];  // Y2
+  assign y[3] = data & sel[1] & sel[0];  // Y3
 
 endmodule
