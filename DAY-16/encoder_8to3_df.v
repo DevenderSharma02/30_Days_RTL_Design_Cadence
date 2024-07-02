74073f1c1b191586b37714da2d1949c37ea63ec7
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-07-02 17:58:48 +0530

    Update encoder_8to3_df.v

--- DAY-16/encoder_8to3_df.v
+++ DAY-16/encoder_8to3_df.v
@@ -6,9 +6,9 @@ module encoder_8to3(
     output [2:0] y
 );
 
-  assign y[0] = in[1] | in[3] | in[5] | in[7];
-  assign y[1] = in[2] | in[3] | in[6] | in]7];
-  assign y[2] = in[4] | in[5] | in[6] | in[7];
+    assign y[0] = (in[1] | in[3] | in[5] | in[7] ) & en;
+    assign y[1] = (in[2] | in[3] | in[6] | in]7] ) & en;
+    assign y[2] = (in[4] | in[5] | in[6] | in[7] ) & en;
 
 endmodule 
       
