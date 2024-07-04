09e66e884901f3d5e86af143fe69d6bff0b57234
DEVENDER_SHARMA <devendersharmaa7@gmail.com>
2024-07-04 16:28:32 +0530

    Add files via upload

---
+++ DAY-21/d_ff_async_rst.v
@@ -0,0 +1,13 @@
+module d_ff_async_reset(clk,rst_n,d,q);
+input clk,rst_n;
+input d;
+output reg q;
+
+
+always @(posedge clk or negedge rst_n)begin
+if(!rst_n)
+      q <= 1'b0;
+else
+     q <= d;
+end
+endmodule
